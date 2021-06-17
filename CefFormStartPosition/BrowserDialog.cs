deea37cb14b8ea664b5c49ec5323656df4ccf7bd
Ben Wiles <ben.wiles@deswik.com>
2021-06-17 11:03:43 +1000

    Example application for CEF

---
+++ CefFormStartPosition/BrowserDialog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp.WinForms;
+
+namespace CefFormStartPosition
+{
+    public partial class BrowserDialog : Form
+    {
+        public BrowserDialog()
+        {
+            InitializeComponent();
+
+            // Removing this line will cause the dialog to consistently render correctly
+            StartPosition = FormStartPosition.CenterParent;
+
+            InitializeCefSharp();
+
+            Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Action closeAction = Close;
+                Invoke(closeAction);
+            });
+        }
+
+        private void InitializeCefSharp()
+        {
+            Uri indexUri = new Uri(Path.Combine(new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName, "index.html"));
+            ChromiumWebBrowser chromiumWebBrowser = new ChromiumWebBrowser(indexUri.AbsoluteUri);
+            Controls.Add(chromiumWebBrowser);
+        }
+    }
+}
---
+++ CefFormStartPosition/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CefFormStartPosition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            // It seems to be important that this form is larger than the dialog it launches
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new BrowserDialog().ShowDialog(this);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                new BrowserDialog().ShowDialog(this);
+            }
+        }
+    }
+}
